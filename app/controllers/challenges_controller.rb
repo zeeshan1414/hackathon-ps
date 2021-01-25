f04ca9516c8ae88ef3bb94823d9a78ff76d7f4cd
Zeeshan Hussain <zhussain@plansource.com>
2021-01-25 10:13:40 +0530

    Add Challenge form and Challenge list table

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -2,6 +2,7 @@ class ChallengesController < ApplicationController
   before_action :authenticate_employee!
 
   def index
+    @challenge = Challenge.new
     @challenges = Challenge.all
 
     respond_to do |format|
@@ -14,31 +15,35 @@ class ChallengesController < ApplicationController
     @challenge = Challenge.new(challenge_params)
     @challenge.employee = current_employee
 
-    if @challenge.save
-      format.html { redirect_to challenges_url, notice: 'Challenge was successfully created.' }
-      format.json { render json: @challenge, status: :created, location: @challenge }
-    else
-      format.html { redirect_to challenges_url, alert: 'Challenge was not successfully created.' }
-      format.json { render json: @challenge.errors, status: :unprocessable_entity }
+    respond_to do |format|
+      if @challenge.save
+        format.html { redirect_to challenges_path, notice: 'Challenge was successfully created.' }
+        format.json { render json: @challenge, status: :created, location: @challenge }
+      else
+        format.html { redirect_to challenges_path, alert: 'Challenge was not successfully created.' }
+        format.json { render json: @challenge.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   def upvote
     @challenge = Challenge.find(params[:id])
-    @challenge.votes += 1 unless @challenge.employee == current_user
+    @challenge.votes += 1
 
-    if @challenge.save
-      format.html { redirect_to challenges_url, notice: 'Challenge was successfully upvoted.' }
-      format.json { render json: @challenge, status: :created, location: @challenge }
-    else
-      format.html { redirect_to challenges_url, alert: 'Challenge was not successfully upvoted.' }
-      format.json { render json: @challenge.errors, status: :unprocessable_entity }
+    respond_to do |format|
+      if @challenge.employee != current_employee && @challenge.save
+        format.html { redirect_to challenges_url, notice: 'Challenge was successfully upvoted.' }
+        format.json { render json: @challenge, status: :created, location: @challenge }
+      else
+        format.html { redirect_to challenges_url, alert: 'Challenge was not successfully upvoted.' }
+        format.json { render json: @challenge.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   def collaborate
     @challenge = Challenge.find(params[:id])
-    @challenge.collaborators << current_user unless @challenge.employee == current_user
+    @challenge.collaborators << current_employee unless @challenge.employee == current_employee
 
     respond_to do |format|
       format.html { render :index, notice: 'Successfully collaborated in Challenge' }
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :challenges, only: %w(index create)
   devise_for :employees
 
+  put 'challenges/upvote/:id', to: 'challenges#upvote', as: 'challenge_upvote'
+  post 'challenges/collaborate/:id', to: 'challenges#collaborate', as: 'challenge_collaborate'
+
   root to: 'challenges#index'
 end
