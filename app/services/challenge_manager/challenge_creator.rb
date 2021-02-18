7795ff085a22faddb4267d83d3d558a36b616189
Zeeshan Hussain <zhussain@plansource.com>
2021-02-18 17:22:37 +0530

    fixed a typo in the challenge_creator

--- app/services/challenge_manager/challenge_creator.rb
+++ app/services/challenge_manager/challenge_creator.rb
@@ -2,10 +2,11 @@ module ChallengeManager
   class ChallengeCreator < ApplicationService
     def initialize(challenge_params, current_employee)
       @challenge = Challenge.new(challenge_params)
+      @current_employee = current_employee
     end
   
     def call
-      @challenge.employee = current_employee
+      @challenge.employee = @current_employee
       @challenge
     end
   end
